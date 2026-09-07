d6e487131d5d02059a8fddec5132e8bed0b53b7a
bxljoy <259500313@qq.com>
2026-09-08 00:16:02 +0200

    feat: scaffold contract-first inventory service

---
+++ inventory-service/src/main/java/com/commercelab/inventory/InventoryServiceApplication.java
@@ -0,0 +1,20 @@
+package com.commercelab.inventory;
+
+import java.time.Clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
+}
